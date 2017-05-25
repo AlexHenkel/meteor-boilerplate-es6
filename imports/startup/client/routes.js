8ed06df757316422e619095646431626f10cd743
Alejandro Henkel <alehenkel17@gmail.com>
2017-05-25 12:37:44 -0500

    clean template and add dependencies

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -2,15 +2,16 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 // Import needed templates
-import '../../ui/layouts/body/body.js';
-import '../../ui/pages/home/home.js';
+import '../../ui/layouts/body/Landing_body';
+import '../../ui/layouts/body/App_body'
+import '../../ui/pages/landing/home.js';
 import '../../ui/pages/not-found/not-found.js';
 
 // Set up all routes in the app
 FlowRouter.route('/', {
-  name: 'App.home',
+  name: 'Landing.home',
   action() {
-    BlazeLayout.render('App_body', { main: 'App_home' });
+    BlazeLayout.render('Landing_body', { main: 'Landing_home' });
   },
 });
 
